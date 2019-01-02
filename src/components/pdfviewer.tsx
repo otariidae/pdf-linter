6ac01fcff3d9b90f70f4e981b24a8a5ba1203941
otariidae <otariidae@users.noreply.github.com>
2019-01-03 01:08:08 +0900

    refactor: use React Hooks

--- src/components/pdfviewer.tsx
+++ src/components/pdfviewer.tsx
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent } from "react"
+import React, { useState, createRef, useEffect } from "react"
 import styled from "styled-components"
 import getPDFDoc, { forEachPage } from "../pdf"
 import { PDFPageProxy } from "pdfjs-dist"
@@ -6,60 +6,46 @@ import { PDFPageProxy } from "pdfjs-dist"
 interface PDFViewerProp {
   file: File
 }
-interface PDFViewerState {
-  pages: PDFPageProxy[]
+
+const PDFViewer = (props: PDFViewerProp) => {
+  const [pages, setPages] = useState<PDFPageProxy[] | null>(null)
+  if (!pages) {
+    getPDFDoc(props.file)
+      .then(doc => {
+        const promises = forEachPage(doc) as Iterable<
+          PromiseLike<PDFPageProxy>
+        >
+        return Promise.all(promises)
+      })
+      .then(pages => {
+        setPages(pages)
+      })
+    return <p>LOADING</p>
+  }
+  return (
+    <PDFPageList>
+      {pages.map((page, i) => (
+        <li key={i}>
+          <PDFPage page={page} />
+        </li>
+      ))}
+    </PDFPageList>
+  )
 }
 
-export default class PDFViewer extends Component<
-  PDFViewerProp,
-  PDFViewerState
-> {
-  constructor(props: PDFViewerProp) {
-    super(props)
-    this.state = {
-      pages: null
-    }
-  }
-  render() {
-    if (!this.state.pages) {
-      getPDFDoc(this.props.file)
-        .then(doc => {
-          const promises = forEachPage(doc) as Iterable<
-            PromiseLike<PDFPageProxy>
-          >
-          return Promise.all(promises)
-        })
-        .then(pages => {
-          this.setState({
-            pages: pages
-          })
-        })
-      return <p>LOADING</p>
-    }
-    const pages = this.state.pages
-    return (
-      <PDFPageList>
-        {pages.map((page, i) => (
-          <li key={i}>
-            <PDFPage page={page} />
-          </li>
-        ))}
-      </PDFPageList>
-    )
-  }
-}
+export default PDFViewer
 
-interface PDFPageProp {
   page: PDFPageProxy
 }
 
-class PDFPage extends PureComponent<PDFPageProp> {
-  constructor(props: PDFPageProp) {
-    super(props)
-    this.renderPDF = this.renderPDF.bind(this)
-  }
-  renderPDF(canvas: HTMLCanvasElement) {
-    const page = this.props.page
+export const PDFPage = (props: PDFPageProp) => {
+  const ref = createRef<HTMLCanvasElement>()
+  useEffect(() => {
+    const canvas = ref.current
+    if (canvas === null) {
+      return
+    }
+    const page = props.page
     const viewport = page.getViewport(1.0)
     canvas.width = viewport.width
     canvas.height = viewport.height
@@ -68,10 +54,8 @@ class PDFPage extends PureComponent<PDFPageProp> {
       canvasContext: ctx,
       viewport: viewport
     })
-  }
-  render() {
-    return <canvas style={{ maxWidth: "100%" }} ref={this.renderPDF} />
-  }
+  }, [ref.current])
+  return <canvas style={{ maxWidth: "100%" }} ref={ref} />
 }
 
 const PDFPageList = styled.ul`
