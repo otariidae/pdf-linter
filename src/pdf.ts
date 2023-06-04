444e49e997ea28896f913386ec6fc9a89370a68a
otariidae <otariidae@users.noreply.github.com>
2023-06-05 03:14:36 +0900

    refactor: use TextItem.hasEOL to identify lines

--- src/pdf.ts
+++ src/pdf.ts
@@ -4,7 +4,7 @@ import { createTextlint } from "./textlint-worker-init"
 import { type TextItem } from "pdfjs-dist/types/src/display/api"
 
 export async function lintPDFFile(file: File): Promise<LintResult> {
-  const textPerPage = await getTextListFromPDFFile(file)
+  const textPerPage = await extractTextFromPDFFile(file)
   const textlint = await createTextlint()
   const responses = await Promise.all(
     textPerPage.map((text) => textlint.lintText(text))
@@ -22,12 +22,39 @@ export async function lintPDFFile(file: File): Promise<LintResult> {
   return lintResult
 }
 
-async function getTextListFromPDFFile(file: File): Promise<string[]> {
+export async function extractTextFromPDFFile(file: File): Promise<string[]> {
   const doc = await getPDFDoc(file)
   const pages = await Promise.all(forEachPage(doc))
-  const textList = await Promise.all(pages.map((page) => getTextFromPage(page)))
+  const textList = await Promise.all(
+    pages.map((page) => extractTextFromPage(page))
+  )
   console.log(textList)
   return textList
+}
+
+async function extractTextFromPage(
+  pdfPage: pdfjs.PDFPageProxy
+): Promise<string> {
+  let text = ""
+  for await (const line of extractLinesFromPage(pdfPage)) {
+    text += line + "\n"
+  }
+  return text.replace(/\0/g, "")
+}
+
+async function* extractLinesFromPage(pdfPage: pdfjs.PDFPageProxy) {
+  const textContent = await pdfPage.getTextContent()
+  const texts = textContent.items.filter(
+    (item): item is TextItem => "str" in item
+  )
+  let line = ""
+  for (const text of texts) {
+    line += text.str
+    if (text.hasEOL) {
+      yield line
+      line = ""
+    }
+  }
 }
 
 async function getPDFDoc(file: File): Promise<pdfjs.PDFDocumentProxy> {
@@ -44,36 +71,7 @@ function* forEachPage(
   pdfDocument: pdfjs.PDFDocumentProxy
 ): Iterable<PromiseLike<pdfjs.PDFPageProxy>> {
   for (let i = 1; i <= pdfDocument.numPages; i++) {
-    const page = pdfDocument.getPage(i) as PromiseLike<pdfjs.PDFPageProxy>
+    const page = pdfDocument.getPage(i)
     yield page
-  }
-}
-
-async function getTextFromPage(pdfPage: pdfjs.PDFPageProxy): Promise<string> {
-  const textContent = await pdfPage.getTextContent()
-  const texts = textContent.items.filter(
-    (item): item is TextItem => "str" in item
-  )
-  let text = ""
-  if (texts.length !== 0) {
-    text += texts[0].str
-  }
-  for (const [pre, cur] of slidingWindows2(texts)) {
-    if (pre.transform[5] === cur.transform[5]) {
-      text += cur.str
-      continue
-    }
-    text += "\n" + cur.str
-  }
-  return text.replace(/\0/g, "")
-}
-
-/**
- * inspired by Deno collections slidingWindows
- * https://deno.land/std@0.107.0/collections/mod.ts?s=slidingWindows
- */
-function* slidingWindows2<T>(array: readonly T[]): Iterable<[T, T]> {
-  for (let i = 0; i < array.length - 1; i++) {
-    yield [array[i], array[i + 1]]
   }
 }
