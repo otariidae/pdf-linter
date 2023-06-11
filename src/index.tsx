b8fbb7eb4f25b576a88de8a09893022d91dceb06
otariidae <otariidae@users.noreply.github.com>
2023-06-11 17:01:05 +0900

    refactor: set global styles in application code

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react"
 import ReactDOM, { render } from "react-dom"
 import { RecoilRoot } from "recoil"
+import { Global } from "@emotion/react"
 import App from "./components/app"
 
 if (process.env.NODE_ENV !== "production") {
@@ -11,6 +12,18 @@ if (process.env.NODE_ENV !== "production") {
 render(
   <StrictMode>
     <RecoilRoot>
+      <Global
+        styles={{
+          body: {
+            margin: 0,
+            height: "100dvh",
+            overflow: "hidden",
+          },
+          "#app": {
+            height: "100%",
+          },
+        }}
+      />
       <App />
     </RecoilRoot>
   </StrictMode>,
