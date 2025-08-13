2d22cdd7a23087320e0ded9e3702d7f1c920cf01
otariidae <otariidae@users.noreply.github.com>
2025-08-13 09:30:23 +0900

    feat: 3-column layout

--- src/components/app.spec.tsx
+++ src/components/app.spec.tsx
@@ -35,23 +35,24 @@ test("should show PDF text content, lint errors, and lint rules", async ({
   // should show lint errors
   const errorCount = component.getByText("7 errors")
   await errorCount.waitFor({ state: "visible" })
+
   expect(component.getByText("二重否定: 〜なくもない")).toBeVisible()
+  const noDoubleNegativeRule = component.getByText(
+    "ja-technical-writing/no-double-negative-ja",
+  )
+  await expect(noDoubleNegativeRule).toBeVisible()
+
   expect(component.getByText("Found kangxi radical: ⾰")).toBeVisible()
+  const noKangxiRadicalsRule = component.getByText(
+    "japanese/no-kangxi-radicals",
+  )
+  await expect(noKangxiRadicalsRule).toBeVisible()
+
   expect(
     component.getByText(
       "「魔法のように」という比喩的表現は現実味に欠ける可能性があります。具体的な仕組みを説明することを検討してください。",
     ),
   ).toBeVisible()
-
-  // should show lint rules
-  const noDoubleNegativeRule = component.getByText(
-    "ja-technical-writing/no-double-negative-ja",
-  )
-  await expect(noDoubleNegativeRule).toBeVisible()
-  const noKangxiRadicalsRule = component.getByText(
-    "japanese/no-kangxi-radicals",
-  )
-  await expect(noKangxiRadicalsRule).toBeVisible()
   const noHypeExpressionRule = component.getByText(
     "@textlint-ja/ai-writing/no-ai-hype-expressions",
   )
@@ -72,14 +73,13 @@ test("should omit lint errors for muted rules", async ({ mount }) => {
   })
 
   // apply mute filter
-  const noDoubleNegativeRuleMuteFiler = component.getByTitle(
-    "Mute ja-technical-writing/no-double-negative-ja rule",
+  const noDoubleNegativeRuleMuteButton = component.getByTitle(
+    "mute ja-technical-writing/no-double-negative-ja rule",
   )
-  await noDoubleNegativeRuleMuteFiler.click()
+  await noDoubleNegativeRuleMuteButton.click()
 
   // should omit lint errors for muted rules
-  const errorCount = component.getByText("6 errors")
-  await errorCount.waitFor({ state: "visible" })
+  await component.getByText("6 errors").waitFor({ state: "visible" })
   expect(component.getByText("二重否定: 〜なくもない")).not.toBeVisible()
   expect(component.getByText("Found kangxi radical: ⾰")).toBeVisible()
   expect(
@@ -87,35 +87,14 @@ test("should omit lint errors for muted rules", async ({ mount }) => {
       "「魔法のように」という比喩的表現は現実味に欠ける可能性があります。具体的な仕組みを説明することを検討してください。",
     ),
   ).toBeVisible()
-})
 
-test("should only show lint errors for the solo rule", async ({ mount }) => {
-  const component = await mount(<App />)
-
-  const fileInput = component.getByLabel("PDFファイルを選択")
-  await fileInput.waitFor({ state: "visible" })
-
-  const pdfFile = await fs.readFile("src/__tests__/example.pdf")
-  await fileInput.setInputFiles({
-    name: "example.pdf",
-    mimeType: "application/pdf",
-    buffer: pdfFile,
-  })
-
-  // apply solo filter
-  const noDoubleNegativeRuleSoleFiler = component.getByTitle(
-    "Solo ja-technical-writing/no-double-negative-ja rule",
+  // unmute the rule
+  const noDoubleNegativeRuleUnmuteButton = component.getByTitle(
+    "unmute ja-technical-writing/no-double-negative-ja rule",
   )
-  await noDoubleNegativeRuleSoleFiler.click()
+  await noDoubleNegativeRuleUnmuteButton.click()
 
-  // should only count lint errors for the solo rule
-  const errorCount = component.getByText("1 error")
-  await errorCount.waitFor({ state: "visible" })
+  // should show lint errors for unmuted rules
+  await component.getByText("7 errors").waitFor({ state: "visible" })
   expect(component.getByText("二重否定: 〜なくもない")).toBeVisible()
-  expect(component.getByText("Found kangxi radical: ⾰")).not.toBeVisible()
-  expect(
-    component.getByText(
-      "「魔法のように」という比喩的表現は現実味に欠ける可能性があります。具体的な仕組みを説明することを検討してください。",
-    ),
-  ).not.toBeVisible()
 })
--- src/components/lintresultviewer.tsx
+++ src/components/lintresultviewer.tsx
@@ -1,48 +1,165 @@
 import { css } from "@emotion/css"
+import type { TextlintRuleSeverityLevel } from "@textlint/kernel"
 import type { FC } from "react"
 import type { LintMessage } from "../type"
 
-const tableStyle = css`
-  table-layuot: fixed;
-  width: 100%;
+const mutedRuleListStyle = css`
+  list-style-type: none;
+  margin-block: 0;
+  padding-inline-start: 0;
+`
 
-  th,
-  td {
-    padding: 2px 4px;
+const chipStyle = css`
+  display: inline-grid;
+  align-items: center;
+  grid-template-columns: 1fr 1.5rem;
+  grid-template-areas: "content icon";
+  padding: 0.2rem 0.2rem 0.2rem 0.75rem;
+  border: 1px solid black;
+  border-radius: 2rem;
+  margin-inline-end: 0.5rem;
 
-    &:nth-child(-n + 3) {
-      width: 1.5em;
-    }
+  > .material-symbols-outlined {
+    grid-area: icon;
+    padding: 0;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
   }
 `
 
-interface LintResultViewerProps {
-  lintResult: LintMessage[]
+const chipContentStyle = css`
+  grid-area: content;
+  vertical-align: middle;
+  font-size: 0.75rem;
+`
+
+const lintMessageListStyle = css`
+  list-style-type: none;
+  margin-block: 0;
+  padding-inline-start: 0;
+`
+
+const lintMessageListItemStyle = css`
+  display: grid;
+  padding-block: 0.625rem;
+  grid-template-columns: 1.75rem 1fr;
+  grid-template-areas: "icon content";
+`
+
+const lintMessageContentStyle = css`
+  > p {
+    margin: 0;
+  }
+`
+
+const lineLinkStyle = css`
+  margin-inline-start: 0.5em;
+  font-size: 0.8rem;
+`
+
+const ruleIdStyle = css`
+  color: #333;
+  font-size: 0.8em;
+`
+
+const muteButtonStyle = css`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`
+
+function ServerityIcon({ severity }: { severity: TextlintRuleSeverityLevel }) {
+  switch (severity) {
+    case 0:
+      return (
+        <span
+          className="material-symbols-outlined"
+          style={{ color: "royalblue" }}
+        >
+          info
+        </span>
+      )
+    case 1:
+      return (
+        <span className="material-symbols-outlined" style={{ color: "orange" }}>
+          warning
+        </span>
+      )
+    case 2:
+      return (
+        <span className="material-symbols-outlined" style={{ color: "red" }}>
+          dangerous
+        </span>
+      )
+  }
 }
 
-const LintResultViewer: FC<LintResultViewerProps> = ({ lintResult }) => (
-  <table className={tableStyle}>
-    <thead>
-      <tr>
-        <th>page</th>
-        <th>line</th>
-        <th>column</th>
-        <th>message</th>
-      </tr>
-    </thead>
-    <tbody>
-      {lintResult.map((message, i) => (
-        <tr key={i}>
-          <td>{message.page}</td>
-          <td>
-            <a href={`#p${message.page}-l${message.line}`}>{message.line}</a>
-          </td>
-          <td>{message.column}</td>
-          <td>{message.message}</td>
-        </tr>
+interface LintResultViewerProps {
+  lintResult: LintMessage[]
+  mutedRuleIds: Set<string>
+  muteRule: (ruleId: string) => void
+  unmuteRule: (ruleId: string) => void
+}
+
+const LintResultViewer: FC<LintResultViewerProps> = ({
+  lintResult,
+  mutedRuleIds,
+  muteRule,
+  unmuteRule,
+}) => (
+  <>
+    <ul className={mutedRuleListStyle}>
+      {Array.from(mutedRuleIds).map((ruleId) => (
+        <li key={ruleId} className={chipStyle}>
+          <span className={chipContentStyle}>{ruleId}</span>
+          <button
+            type="button"
+            title={`unmute ${ruleId} rule`}
+            className="material-symbols-outlined"
+            onClick={() => unmuteRule(ruleId)}
+          >
+            close
+          </button>
+        </li>
       ))}
-    </tbody>
-  </table>
+    </ul>
+    <ol className={lintMessageListStyle}>
+      {lintResult.map((message, i) => (
+        <li key={i} className={lintMessageListItemStyle}>
+          <div style={{ gridArea: "icon" }}>
+            <ServerityIcon severity={message.severity} />
+          </div>
+          <div
+            style={{ gridArea: "content" }}
+            className={lintMessageContentStyle}
+          >
+            <p>
+              {message.message}
+              <a
+                className={lineLinkStyle}
+                href={`#p${message.page}-l${message.loc.start.line}`}
+              >
+                [Pg {message.page}, Ln {message.loc.start.line}, Col{" "}
+                {message.loc.start.column}]
+              </a>
+            </p>
+            <p>
+              <span className={ruleIdStyle}>{message.ruleId}</span>
+              <button
+                type="button"
+                title={`mute ${message.ruleId} rule`}
+                className={muteButtonStyle}
+                onClick={() => muteRule(message.ruleId)}
+              >
+                mute this rule
+              </button>
+            </p>
+          </div>
+        </li>
+      ))}
+    </ol>
+  </>
 )
 
 export default LintResultViewer
